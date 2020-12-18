f1f6645511513e11349f0033929035fc3d9b8d53
Mário César Melon Bomfim <mariocesarbomfim@gmail.com>
2020-12-18 01:09:19 -0300

    Add files via upload

---
+++ code.cc
@@ -0,0 +1,26 @@
+programa teste {
+
+  int x,y,z;
+  char abc;
+  real pi := 3.1415;
+
+  se (2 > 3) entao {
+    x := 2;
+  }
+
+  senao {
+    y := 3;
+  }
+
+
+  enquanto(x > 2) {
+    z := 10; 
+  }
+
+  faca {
+
+    pi := 3*3;  
+
+  } enquanto( z < 10 );
+
+}
